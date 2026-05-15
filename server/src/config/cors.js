cc7d899be8fe3e4bf11771d7c779ce76e58bf041
Akhatar Raza Mirza <akhtarrazamirza111@gmail.com>
2026-05-15 16:40:07 +0530

    changing cors configration

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -3,6 +3,7 @@ const defaultClientOrigins = [
     "http://127.0.0.1:5173",
     "http://localhost:4173",
     "http://127.0.0.1:4173",
+    "https://pollboard-opal.vercel.app/"
 ];
 
 export const getAllowedOrigins = () => {
